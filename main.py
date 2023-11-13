bbe284247e257e9bba753b010ddfaa8a57f0c12e
Dominick Haggerty (Class of 2025) <250700dh@salesianum.org>
2023-11-13 10:52:04 -0500

    Main added

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+def main():
+    generrate_random_number()
+
+def generrate_random_number():
+    number = random.randint(1,100)
+    return number 
+
+
+if __name__ == "__main__":
+    main()
