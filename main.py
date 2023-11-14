87b8702ea32cef75b6ab69b3cce9a47bff1ca95d
dlorentz1212 <143014074+dlorentz1212@users.noreply.github.com>
2023-11-14 14:55:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import random
 
-def main():
-    generrate_random_number()
 
-def generrate_random_number():
-    number = random.randint(1,100)
-    return number 
+number = random.randint(1, 100)
 
 
-if __name__ == "__main__":
-    main()
+# working on while loop for the logic part based off Dom's original game code.
+def player_guess():
+
+    for x in range(30):
+        player = int(input("guess a number:"))
+
+    if (player == number):
+        print("Correct!")
+
+    elif (player > number):
+        print("Lower")
+
+    else:
+        print("Higher")
+
+    player = int(input("guess another number:"))
+
+    if (player == number):
+        print("That is the number")
+
+    elif (player > number):
+        print("lower")
+
+    else:
+        print("higher")
+
+    player = int(input("guess another number:   "))
+
+    if (player == number):
+        print("That is the number")
+
+    elif (player > number):
+        print("You lose")
+
+    else:
+        print("You lose")
+
+    print(number)
